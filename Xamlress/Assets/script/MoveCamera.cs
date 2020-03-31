9bdbe5e9c9ef7adfba5dd6c83ec3d1db8278f9e7
Quinten36 <codestuff4all@gmail.com>
2020-03-31 12:43:03 +0200

    first time upload

---
+++ Xamlress/Assets/script/MoveCamera.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+
+    public Transform player;
+    public Vector3 offset;
+
+    void Update() {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Xamlress/Assets/script/agent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class agent : MonoBehaviour
+{
+
+    private NavMeshAgent agentN;
+
+    public Transform destination;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agentN = GetComponent<NavMeshAgent>();
+        //agentN.SetDestination(destination.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if (Input.GetMouseButton(0))
+        {
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                agentN.SetDestination(hit.point);
+            }
+
+        }
+    }
+}
