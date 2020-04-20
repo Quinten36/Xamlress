e0b09b2b458f5bb805daf8a40f81f49eb21c02f3
Quinten36 <codestuff4all@gmail.com>
2020-04-20 14:01:57 +0200

    sdf

---
+++ Assets/teleportTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class teleportTrigger : MonoBehaviour
+{
+
+    // teleport naar sublevel
+
+    // misschien een fade
+
+    public string levelName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(levelName);
+        // SceneManager.LoadScene(levelName);
+    }
+}
